e906dab51eeee4409fe22180a4668089d2a901b1
abarani30 <alisattarbarani@outlook.com>
2022-11-23 14:10:35 +0300

    ali

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -17,47 +17,32 @@ const Profile = () => {
     
     return (
         <Layout title="Profile" content="The user profile page">
-            <div class="container rounded bg-white mt-5 mb-5">
-                <div class="row">
+            <div class="container rounded mt-0 mb-5">
+                <div class="row d-flex justify-content-center align-items-center">
                    
-                    <div class="col-md-4 border-right">
-                        <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                            <img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" />
-                            <span class="font-weight-bold">Edogaru</span>
-                            <span class="text-black-50">edogaru@mail.com.my</span>
+                    <div class="col-md-4 picture-side">
+                        <div class="d-flex flex-column align-items-center text-center p-0">
+                            <img class="rounded-circle mt-5" width="150px" src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+                            <h5 class="mt-3">{user !== null && user.username}</h5>
+                            <span class="text-black-50">{user !== null && user.email}</span>
                             <span> </span>
                         </div>
                     </div>
 
                     <div class="col-md-6 border-right">
-                        <div class="p-3 py-5">
-                            
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <h4 class="text-right">Profile Settings</h4>
-                            </div>
+                        <div class="p-0">
                            
-                            <div class="row mt-2">
-                                <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value="" /></div>
-                                <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname" /></div>
+                            <div class="row mt-4">
+                                <div class="col-md-6"><label class="form-label">Username:</label><input type="text" class="form-control" placeholder="John Doe" value={user !== null ? user.username : ""} /></div>
+                                <div class="col-md-6"><label class="form-label">Email:</label><input type="text" class="form-control" value={user !== null ? user.email : ""} placeholder="john.doe@gmail.com" /></div>
                             </div>
                            
                             <div class="row mt-3">
-                                <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value="" /></div>
-                                <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value="" /></div>
-                                <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                                <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                                <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                                <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                                <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value="" /></div>
-                                <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value="" /></div>
+                                <div class="col-md-12 mb-3"><label class="form-label">Bio:</label><textarea class="form-control" rows="3" placeholder="Enter your bio"></textarea></div>
+                                <div class="col-md-12"><label class="form-label">Address:</label><input type="text" class="form-control" placeholder="enter address line 1" value="" /></div>
                             </div>
                            
-                            <div class="row mt-3">
-                                <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value="" /></div>
-                                <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state" /></div>
-                            </div>
-                            
-                            <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                            <div class="mt-5 text-center"><button class="btn btn-primary btn-sm w-100" type="button">Update</button></div>
                         </div>
                     
                     </div>
